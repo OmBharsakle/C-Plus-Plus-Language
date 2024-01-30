9469f1b3eeef06789c373b3dc3377e515c7c8b93
OmBharsakle <149371245+OmBharsakle@users.noreply.github.com>
2024-01-30 01:24:49 -0800

    Add files via upload

---
+++ PR. 1 The New Era/02.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Time
+{
+	public :
+	int hh,mm,ss;	
+	
+};
+main()
+{
+	Time t;
+	
+	int hh,mm,ss;
+	
+	cout << "Enter Your Second : ";
+	cin >> ss;
+	
+	t.hh = ss/3600;
+	t.mm = (hh%3600)/60;
+	t.ss = (mm%3600)/60;
+	
+	cout << t.hh << ":" << t.mm << ":" << t.ss ;
+	
+}
