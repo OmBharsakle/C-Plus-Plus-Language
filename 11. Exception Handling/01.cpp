16fd341e80913a6113420b6acea98d9abb74e29d
OmBharsakle <149371245+OmBharsakle@users.noreply.github.com>
2024-02-23 13:57:01 +0530

    Update 01.cpp

--- 11. Exception Handling/01.cpp
+++ 11. Exception Handling/01.cpp
@@ -1,8 +1,8 @@
 //Q.1 Write a Program to implement exception handling mechanism for different types of scenarios:
 //- a number cannot be divided by zero
 //- a person cannot be able to vote if his/her age is less than 18
-//- a password cannot be validated if it doesn’t contains an uppercase letter
-#include<iostream>
+//- a password cannot be validated if it doesnâ€™t contains an uppercase letter
+#include<iostream> 
 using namespace std;
 
 void division()
